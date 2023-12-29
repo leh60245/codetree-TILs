4004353fdcc59d5b65ddefa3947b36441369df91
LeeEuiHyoung <55617010+leh60245@users.noreply.github.com>
2023-12-29 23:33:14 +0900

    Title: 알파벳에 따른 평가, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231229/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,3 @@
+string = input()
+answer = "Superior" if string == "S" else "Excellent" if string == "A" else "Good" if string == "B" else "Usually" if string == "C" else "Effort" if string == "D" else "Failure"
+print(answer)
