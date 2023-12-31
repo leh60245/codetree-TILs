f4be78230fe91136504fbf759cdd8521462434ef
LeeEuiHyoung <55617010+leh60245@users.noreply.github.com>
2023-12-31 23:02:26 +0900

    Title: 비교 연산, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231231/비교 연산/comparison-operator.py
@@ -0,0 +1,8 @@
+a, b = map(int, input().split())
+an1 = 1 if a >= b else 0
+an2 = 1 if a > b else 0
+an3 = 1 if a <= b else 0
+an4 = 1 if a < b else 0
+an5 = 1 if a == b else 0
+an6 = 1 if a != b else 0
+print(f"{an1}\n{an2}\n{an3}\n{an4}\n{an5}\n{an6}")
