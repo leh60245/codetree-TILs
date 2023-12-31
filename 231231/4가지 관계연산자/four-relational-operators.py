6a64a154e7d01172a5eccd0bc37c282ce122ec8d
LeeEuiHyoung <55617010+leh60245@users.noreply.github.com>
2023-12-31 23:04:17 +0900

    Title: 4가지 관계연산자, Time: 175ms, Memory: 24MB, Status: Passed - CodeTree

--- 231231/4가지 관계연산자/four-relational-operators.py
+++ 231231/4가지 관계연산자/four-relational-operators.py
@@ -2,5 +2,5 @@ a, b = map(int, input().split())
 an1 = 1 if a >= b else 0
 an2 = 1 if a > b else 0
 an3 = 1 if a <= b else 0
-an4 = 1 if a < b else0
+an4 = 1 if a < b else 0
 print(f"{an1}\n{an2}\n{an3}\n{an4}")
