f7c3844f3dbd64bc9c548979a5bdd8e181e36632
LeeEuiHyoung <55617010+leh60245@users.noreply.github.com>
2024-10-06 01:57:01 +0900

    Title: 1차원 젠가, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 241006/1차원 젠가/jenga-1d.py
+++ 241006/1차원 젠가/jenga-1d.py
@@ -5,8 +5,18 @@ for _ in range(n):
 s1, e1 = map(int, input().split())
 s2, e2 = map(int, input().split())
 
-arr = arr[:s1-1] + arr[e1-1:]
-tmp = arr[:s1-1] + arr[e1-1:]
+tmp = []
+for i in range(len(arr)):
+    if (s1 - 1 <= i) and (i < e1):
+        continue
+    tmp.append(arr[i])
+arr = tmp
+
+tmp = []
+for i in range(len(arr)):
+    if (s2 - 1 <= i) and (i < e2):
+        continue
+    tmp.append(arr[i])
 
 print(len(tmp))
 for i in range(len(tmp)):
