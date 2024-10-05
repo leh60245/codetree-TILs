a2ba3bb2b476cea07d724bb4b18ba064ac6e8751
LeeEuiHyoung <55617010+leh60245@users.noreply.github.com>
2024-10-05 22:29:18 +0900

    Title: 트로미노, Time: 184ms, Memory: 25MB, Status: Passed - CodeTree

--- 241005/트로미노/tromino.py
+++ 241005/트로미노/tromino.py
@@ -25,15 +25,15 @@ def chk6(x, y):
 
 answer = 0
 for i in range(n):
-    for j in range(n-2):
+    for j in range(m-2):
         answer = max(answer, chk5(i, j))
 
 for i in range(n-2):
-    for j in range(n):
+    for j in range(m):
         answer = max(answer, chk6(i, j))
 
 for i in range(n-1):
-    for j in range(n-1):
+    for j in range(m-1):
         answer = max(answer, chk1(i, j), chk2(i, j), chk3(i, j), chk4(i, j))
 
 print(answer)
