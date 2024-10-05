72bdea4bc5d4d184e8b0383343880f0a9e54f624
LeeEuiHyoung <55617010+leh60245@users.noreply.github.com>
2024-10-05 22:25:55 +0900

    Title: 트로미노, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241005/트로미노/tromino.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+def chk1(x, y):
+    return arr[x][y] + arr[x+1][y] + arr[x][y+1]
+
+def chk2(x, y):
+    return arr[x][y] + arr[x+1][y] + arr[x+1][y+1]
+
+def chk3(x, y):
+    return arr[x][y] + arr[x+1][y+1] + arr[x][y+1]
+
+def chk4(x, y):
+    return arr[x][y+1] + arr[x+1][y] + arr[x+1][y+1]
+
+def chk5(x, y):
+    return arr[x][y] + arr[x][y+1] + arr[x][y+2]
+
+answer = 0
+for i in range(n):
+    for j in range(n-2):
+        answer = max(answer, chk5(i, j))
+
+for i in range(n-1):
+    for j in range(n-1):
+        answer = max(answer, chk1(i, j), chk2(i, j), chk3(i, j), chk4(i, j))
+
+print(answer)
