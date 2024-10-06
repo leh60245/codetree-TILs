fce3ad3bd482e7718dcf35d2190bcc5ebdc938da
LeeEuiHyoung <55617010+leh60245@users.noreply.github.com>
2024-10-06 16:12:46 +0900

    Title: 벽 짚고 미로 탈출하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241006/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -0,0 +1,54 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+x, y = tuple(map(int, input().split()))
+x, y = x - 1, y - 1
+arr = []
+for _ in range(n):
+    st = list(input())[:-1]
+    arr.append(st)
+
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]  # 시계 방향
+idx = 0
+
+
+def in_box(x, y):
+    return x >= 0 and x < n and y >= 0 and y < n
+
+
+def is_wall(gx, gy):
+    return arr[gx][gy] == "#"
+
+
+T = 0
+v = []
+while True:
+    mx, my = x + dx[idx % 4], y + dy[idx % 4]
+    # print("look", mx, my)
+    if (x, y, mx, my) in v:
+        # print("never end")
+        T = -1
+        break
+    v.append((x, y, mx, my))
+    if is_wall(mx, my):
+        idx -= 1
+        # print("turn left")
+        continue
+    if not in_box(mx, my):
+        T += 1
+        # print("finish")
+        break
+    wx, wy = mx + dx[(idx + 1) % 4], my + dy[(idx + 1) % 4]
+    if is_wall(wx, wy):
+        x, y = mx, my
+        T += 1
+        # print("go to front")
+    else:
+        x, y = wx, wy
+        T += 2
+        idx += 1
+        # print("turn right")
+
+print(T)
